9392715c15ebb31a423847d4c5dba165116413bf
yurlovakristina <yurlovakristina9@gmail.com>
2025-04-05 01:12:29 +0200

    Update Storage.ts

--- src/classes/Storage.ts
+++ src/classes/Storage.ts
@@ -1,18 +1,17 @@
-// Универсальный класс Storage
+
 export class Storage<T> {
     private value: T | null = null;
   
-    // Добавить значение в хранилище
+  
     add(value: T): void {
       this.value = value;
     }
   
-    // Извлечь значение из хранилища
     get(): T | null {
       return this.value;
     }
   
-    // Очистить сохранённое значение
+   
     clear(): void {
       this.value = null;
     }
