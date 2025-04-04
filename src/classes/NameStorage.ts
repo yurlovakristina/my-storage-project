229946e5217f6dea669833e307a8307c38d0d4c5
yurlovakristina <yurlovakristina9@gmail.com>
2025-04-05 01:12:14 +0200

    Update NameStorage.ts

--- src/classes/NameStorage.ts
+++ src/classes/NameStorage.ts
@@ -1,22 +1,22 @@
-// Импорт интерфейса HasName
+
 import { HasName } from "../interfaces/HasName";
 import { Storage } from "./Storage";
 
-// Класс, расширяющий Storage с ограничением на объекты с свойством 'name'
+
 export class NameStorage<T extends HasName> {
   private value: T | null = null;
 
-  // Добавить значение в хранилище
+ 
   add(value: T): void {
     this.value = value;
   }
 
-  // Извлечь значение из хранилища
+ 
   get(): T | null {
     return this.value;
   }
 
-  // Очистить сохранённое значение
+ 
   clear(): void {
     this.value = null;
   }
